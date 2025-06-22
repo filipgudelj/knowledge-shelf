f5a8e078677b0b6c1141a034e7269fc5d896f9f6
Filip Gudelj <filip.gudelj5@gmail.com>
2025-06-23 00:42:56 +0200

    Update dropdown menu for dark mode

--- components/shared/UserDropdown.vue
+++ components/shared/UserDropdown.vue
@@ -23,7 +23,10 @@ const handleLogout = () => {
     </button>
 
     <div v-if="showMenu" class="dropdown__menu">
-      <button class="dropdown__item">Settings</button>
+      <button class="dropdown__item">
+        <div class="dropdown__label">Settings</div>
+        <Icon name="mdi:chevron-right" size="20px" class="dropdown__icon" />
+      </button>
       <button
         @click="handleLogout"
         class="dropdown__item dropdown__item--logout"
@@ -65,17 +68,57 @@ const handleLogout = () => {
   border-radius: $radius-4;
   box-shadow: 0px 2px 12px 3px rgba(0, 0, 0, 0.4);
   background-color: $color-gray-100;
+
+  html.dark & {
+    background-color: $color-gray-900;
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: -5px;
+    right: 10.5px;
+    width: 0;
+    height: 0;
+    border-right: 6px solid transparent;
+    border-bottom: 6px solid $color-gray-100;
+    border-left: 6px solid transparent;
+
+    html.dark & {
+      border-bottom-color: $color-gray-900;
+    }
+  }
 }
 
 .dropdown__item {
-  @include flex(row, flex-start, center);
+  @include flex(row, space-between, center);
   width: 100%;
-  padding-inline: $spacing-2;
+  padding-inline: $spacing-3;
   padding-block: $spacing-2;
   border-radius: $radius-3;
+  transition: all 0.3s ease;
+
+  html.dark & {
+    &:hover {
+      background-color: $color-gray-800;
+    }
+  }
 
   &:hover {
     background-color: $color-gray-200;
+
+    .dropdown__icon {
+      transform: translateX(2px);
+    }
+  }
+}
+
+.dropdown__icon {
+  color: $color-gray-800;
+  transition: transform 0.15s ease;
+
+  html.dark & {
+    color: $color-gray-200;
   }
 }
 
