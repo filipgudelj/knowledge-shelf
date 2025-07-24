65cd8c842fc41cf8dfd389d56ccda937ed702bb3
Filip Gudelj <filip.gudelj5@gmail.com>
2025-07-24 05:39:19 +0200

    Fix theme flashes

---
+++ middleware/theme.global.ts
@@ -0,0 +1,16 @@
+import { useDark } from '@vueuse/core'
+
+export default defineNuxtRouteMiddleware((to, from) => {
+  const themeCookie = useCookie('theme-cookie')
+  const isDark = useDark()
+
+  watch(isDark, (newIsDarkValue) => {
+    themeCookie.value = newIsDarkValue ? 'dark' : 'light'
+  })
+
+  useHead({
+    htmlAttrs: {
+      class: themeCookie.value === 'dark' ? 'dark' : '',
+    },
+  })
+})
